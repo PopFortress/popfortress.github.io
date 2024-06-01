7f4ff2f9a5df24df25c6ee6ad7e163be25e1ad12
PopFortress <124672853+PopFortress@users.noreply.github.com>
2024-06-01 20:58:27 +0800

    Add files via upload

---
+++ static/index-easter-egg.js
@@ -0,0 +1,15 @@
+const invoker = document.querySelector('.easter-egg-entrance');
+const container = document.querySelector('.suspage-hl-wrapper');
+
+var counter = 0;
+invoker.addEventListener('click', function() {
+    counter++;
+    if (counter === 8) {
+        const suspage_hl = document.createElement('a');
+        suspage_hl.className = 'suspage-hl';
+        suspage_hl.href = '__info/suspage.html';
+        suspage_hl.textContent = '可疑的页面';
+        container.appendChild(suspage_hl);
+        container.style.marginTop = '50vh';
+    };
+});
