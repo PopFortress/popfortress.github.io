c90b3bb82db766a14a55ea800aacb37fa4fa58ff
PopFortress <popfortress@outlook.com>
2025-07-25 00:35:35 +0800

    enhancement for latex and vanilla craft

--- announcements/vanilla-craft/script.js
+++ announcements/vanilla-craft/script.js
@@ -4,13 +4,28 @@ const showBalance = $('.show-balance');
 const rechargeBtn = $('.recharge');
 const balanceNum = $('.balance-num');
 const passcodeDialog = new mdui.Dialog('#passcode-dialog');
-const loadingDialog = new mdui.Dialog('#loading-dialog', { modal: true, closeOnEsc: false, });
+const loadingDialog = new mdui.Dialog('#loading-dialog', { modal: true, closeOnEsc: false, history: false });
 const accountInput = $('#account-input');
 const submitRecharge = $('#submit-recharge');
-const nameserver = 'https://seep.eu.org/https://api.minecraftservices.com/users/profiles/minecraft/';
+const nameserver = { nameserver: 'https://seep.eu.org/https://api.minecraftservices.com/users/profiles/minecraft/',
+    avatar_server: 'https://api.mineatar.io/face/',
+};
 let balance = localStorage.store_balance ? +localStorage.store_balance : 0;
 const pageTitle = document.title;
 let rechargeAmount;
+const accountAvatar = $('.account-avatar');
+const accountName = $('.account-name');
+const loginBtn = $('.login');
+let passcodeDialogFor = 'login';
+const logoutBtn = $('.logout');
+const servicesContainer = $('.services-container');
+const searchBtn = $('.search-btn');
+const searchInput = $('.search-input');
+let listItems;
+
+if (localStorage.store_auth_username) {
+    handleAuth(localStorage.store_auth_username);
+};
 
 showBalance.onclick = () => {
     mdui.confirm(`Your balance is ${balance} <br> Avaliable credit: ${balance.toFixed(2)}`, 'Your Balance',
@@ -28,6 +43,7 @@ function purchase() {
         (value) => {
             if (value > 0 && Number.isSafeInteger(+value)) {
                 passcodeDialog.open();
+                passcodeDialogFor = 'purchase';
                 rechargeAmount = +value;
             } else {
                 mdui.alert('Invalid input.');
@@ -51,26 +67,137 @@ document.onvisibilitychange = () => {
 
 rechargeBtn.onclick = purchase;
 
-submitRecharge.onclick = async () => {
-    loadingDialog.open();
-    const resp = await fetch(`${nameserver}${accountInput.value}`);
-    const data = await resp.json();
-    loadingDialog.close();
-    if (data.id) {
-        mdui.confirm(`You're purchasing ${rechargeAmount} tokens for account <br> UUID: ${data.id} <br> Name: ${data.name} <br> Would you like to proceed?`,
-            'Confirm Purchase',
-            () => {
-                balance += rechargeAmount;
-                updateBalance();
-                mdui.snackbar('Your purchase has been completed successfully.');
-            },
-        );
-    } else {
-        mdui.alert(data.errorMessage);
-    };
-};
+submitRecharge.onclick = () => { handleAuth(accountInput.value); };
 
 function updateBalance() {
     localStorage.store_balance = balance;
     balanceNum.innerHTML = `Tokens: ${balance}`;
 };
+
+async function handleAuth(username) {
+    loadingDialog.open();
+    const resp = await fetch(`${nameserver.nameserver}${username}`);
+    const data = await resp.json();
+    loadingDialog.close();
+    if (data.id) {
+        if (passcodeDialogFor === 'purchase') {
+            mdui.confirm(`You're purchasing ${rechargeAmount} tokens for account <br> UUID: ${data.id} <br> Name: ${data.name} <br> Would you like to proceed?`,
+                'Confirm Purchase',
+                async () => {
+                    balance += rechargeAmount;
+                    updateBalance();
+                    loadingDialog.open();
+                    await fetch(`${nameserver.nameserver}pizza`);
+                    loadingDialog.close();
+                    mdui.snackbar('Your purchase has been completed successfully.');
+                },
+            );
+        } else {
+            accountAvatar.src = `${nameserver.avatar_server}${data.id}`;
+            accountName.innerHTML = data.name;
+            mdui.snackbar(`Welcome back, ${data.name}!`);
+            loginBtn.innerHTML = '切换帐户';
+            logoutBtn.style.display = 'inline';
+            localStorage.store_auth_username = username;
+        };
+    } else {
+        mdui.alert(data.errorMessage);
+    };
+};
+
+loginBtn.onclick = () => {
+    passcodeDialog.open();
+    passcodeDialogFor = 'login';
+};
+
+logoutBtn.onclick = () => {
+    localStorage.removeItem('store_auth_username');
+    location.reload();
+};
+
+function purchaseItem(title, price) {
+    mdui.prompt('Enter purchasing amount.', 'Purchase for Service', 
+        (value) => {
+            if (value > 0 && value < 101 && Number.isInteger(+value)) {
+                const priceTotal = +value * price;
+                mdui.confirm(`You're purchasing ${value} amounts for service ${title}, which costs ${priceTotal} tokens. <br> Would you like to proceed?`,
+                    'Confirm Purchase',
+                    async () => {
+                        if (priceTotal <= balance) {
+                            balance -= priceTotal;
+                            updateBalance();
+                            loadingDialog.open();
+                            await fetch(`${nameserver.nameserver}pizza`);
+                            loadingDialog.close();
+                            mdui.snackbar('Your purchase has been completed successfully.');
+                        } else {
+                            mdui.alert(`Insufficient balance. You need ${priceTotal - balance} tokens more.`);
+                        };
+                    },
+                );
+            } else {
+                mdui.alert('Invalid input. Amount should be an integer between 1 and 100.');
+            };
+        },
+        (_, dialog) => {dialog.close();},
+        { confirmOnEnter: true, defaultValue: 1}
+    );
+};
+
+async function fetchStoreItems() {
+    const resp = await fetch('./store.json');
+    const data = await resp.json();
+    data.forEach(item => {
+        const itemCard = document.createElement('div');
+        itemCard.className = 'mdui-card';
+        if (item.image) {
+            itemCard.innerHTML += `<div class="mdui-card-media"><img src="${item.image}"/></div>`;
+        };
+        itemCard.innerHTML += 
+        `<div class="mdui-card-primary">
+            <div class="mdui-card-primary-title">${item.title}</div>
+            <div class="mdui-card-primary-subtitle">Tokens Required: ${item.price}</div>
+        </div>
+        
+        <div class="mdui-card-content">${item.description}</div>`;
+        const actionsWrapper = document.createElement('div');
+        actionsWrapper.className = 'mdui-card-actions';
+        const purchaseItemBtn = document.createElement('button');
+        purchaseItemBtn.className = 'mdui-btn mdui-color-theme mdui-ripple buy-item-btn'
+        purchaseItemBtn.onclick = () => { purchaseItem(item.title, item.price) };
+        purchaseItemBtn.innerHTML = 'add to cart';
+        actionsWrapper.appendChild(purchaseItemBtn);
+        itemCard.appendChild(actionsWrapper);
+        servicesContainer.appendChild(itemCard);
+    });
+    listItems = document.querySelectorAll('.services-container .mdui-card');
+};
+
+fetchStoreItems();
+
+
+function searchItems() {
+    const searchText = searchInput.value.toLowerCase();
+    listItems.forEach(item => {
+        const title = item.querySelector('.mdui-card-primary-title').textContent.toLowerCase();
+        const desc = item.querySelector('.mdui-card-content').textContent.toLowerCase();
+        if (title.includes(searchText) || desc.includes(searchText)) {
+            item.style.display = 'block';
+        } else {
+            item.style.display = 'none';
+        };
+    });
+    if (!searchText) {
+        listItems.forEach(element => {
+            element.style.display = 'block';
+        });
+    };
+};
+
+searchInput.addEventListener('keydown', (e) => {
+    if (e.key === 'Enter') {
+        searchItems();
+    };
+});
+
+searchBtn.onclick = searchItems;
--- material/latex/script.js
+++ material/latex/script.js
@@ -9,21 +9,38 @@ const expandBtn = $('.expand-btn');
 const viewSettingsWrapper = $('.view-settings');
 const centerCheck = $('.center-checkbox');
 const whiteCheck = $('.white-checkbox');
+const scrollCheck = $('.scroll-checkbox');
 const expnadIcon = $('.expand-icon');
 const resultCard = $('.result-card');
+const loading = $('.loading');
 let latexSource;
 let expanded = false;
 
+function load(state) {
+    if (state) {
+        loading.style.display = 'flex';
+        previewBtn.disabled = true;
+        latexInput.disabled = true;
+        downloadBtn.style.display = 'none';
+    } else {
+        loading.style.display = 'none';
+        previewBtn.disabled = false;
+        latexInput.disabled = false;
+        downloadBtn.style.display = 'inline';
+    };
+};
+
 previewBtn.onclick = async () => {
+    load(true);
     latexSource = `https://www.zhihu.com/equation?tex=${encodeURIComponent(latexInput.value)}`;
     latexImg.src = latexSource;
     const resp = await fetch(`https://api.allorigins.win/get?url=${latexSource}`);
     const data = await resp.json();
     const dataURL = data.contents;
-    downloadBtn.style.display = 'inline';
     downloadBtn.href = dataURL;
     resultContainer.style.display = 'block';
     expandBtn.style.display = 'inline';
+    load(false);
 };
 
 latexInput.oninput = () => {
@@ -59,5 +76,13 @@ whiteCheck.onchange = () => {
         resultCard.style.backgroundColor = '#fff';
     } else {
         resultCard.style.backgroundColor = 'revert-layer';
+    };
+};
+
+scrollCheck.onchange = () => {
+    if (scrollCheck.checked) {
+        resultCard.style.overflow = 'auto';
+    } else {
+        resultCard.style.overflow = 'hidden';
     };
 };
